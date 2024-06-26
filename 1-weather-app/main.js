4ba432be821e1afed9105aa5137136fc2edf0a91
Daniela de Sá <danieladesa01@gmail.com>
2024-06-26 11:00:02 -0300

    fixed syntax error of first project and make the script an external file

---
+++ 1-weather-app/main.js
@@ -0,0 +1,52 @@
+// API variables
+const apiKey = "72a2eed9ea9740d29b896a241c0394cb";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+// HTML variables
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+// Function that requests data to API and uses the response to update the HTML
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appId=${apiKey}`);
+
+    if(response.status ==404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else {
+        var data = await response.json();
+
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " ºC";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if(data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        }
+        else if(data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        }
+        else if(data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzel.png";
+        }
+        else if(data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        }
+        else if(data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        }
+        document.querySelector(".error").style.display = "none";
+        document.querySelector(".weather").style.display = "block";
+    }
+    
+}
+
+// Event listener that awaits for the button to be clicked to then use the function to display weather
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 # WEATHER APP
 [Weather png icons source](https://pngtree.com/element/down?id=NDA0OTkxMg==&type=1&time=1719379127&token=YjFjM2VjZjg0ZTBlZGFhM2E2MDY1NmYwNjAwYmYxY2M=&t=0)
 [Search png icon source](https://pngtree.com/element/down?id=Mzk5NDk4Ng==&type=1&time=1719379986&token=ZjEzMjI3ZWFlMTAzNmQ0YTljNjBlZTU4YjMxNTcxZDE=&t=0)
+[Open Weather API](https://openweathermap.org/current#name)
 
 New things I learned from it: 
 * **CSS**: linear gradient
@@ -13,3 +14,4 @@ Extra things I added:
 * Changed the way the weather icons are displayed to a more dynamic manner
 * Added extra information to weather card
 * Added option to visualize more than one city/card
+* Fixed bug for when theres more than a city with the same name
