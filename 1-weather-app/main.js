e23e0ae35276249ffaabfc11656a3de071875256
Daniela de Sá <danieladesa01@gmail.com>
2024-06-26 18:32:36 -0300

    add ons completed

--- 1-weather-app/main.js
+++ 1-weather-app/main.js
@@ -1,15 +1,19 @@
 // API variables
 const apiKey = "72a2eed9ea9740d29b896a241c0394cb";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+var unit = "metric";
+var name;
 
 // HTML variables
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
+const cBtn = document.querySelector(".c");
+const fBtn = document.querySelector(".f");
 
 // Function that requests data to API and uses the response to update the HTML
-async function checkWeather(city) {
-    const response = await fetch(apiUrl + city + `&appId=${apiKey}`);
+async function checkWeather(city, units) {
+    const response = await fetch(apiUrl + city + `&units=${unit}` + `&appId=${apiKey}`);
 
     if(response.status ==404) {
         document.querySelector(".error").style.display = "block";
@@ -19,36 +23,49 @@ async function checkWeather(city) {
         var data = await response.json();
 
         console.log(data);
-
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " ºC";
+        
+        document.querySelector(".img-description").innerHTML = data.weather[0].description;
+        document.querySelector(".city").innerHTML = "in " + data.name  + ", " + data.sys.country;
         document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
-        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-        if(data.weather[0].main == "Clouds") {
-            weatherIcon.src = "images/clouds.png";
+        if (units == "imperial") {
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " ºF";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
+            document.querySelector(".feels-like").innerHTML = "but feels like " + Math.round(data.main.feels_like) + " ºF";
+            document.querySelector(".min").innerHTML = Math.round(data.main.temp_min) + " ºF";
+            document.querySelector(".max").innerHTML = Math.round(data.main.temp_max) + " ºF";
         }
-        else if(data.weather[0].main == "Clear") {
-            weatherIcon.src = "images/clear.png";
+        else {
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " ºC";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+            document.querySelector(".feels-like").innerHTML = "but feels like " + Math.round(data.main.feels_like) + " ºC";
+            document.querySelector(".min").innerHTML = Math.round(data.main.temp_min) + " ºC";
+            document.querySelector(".max").innerHTML = Math.round(data.main.temp_max) + " ºC";
         }
-        else if(data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "images/drizzel.png";
-        }
-        else if(data.weather[0].main == "Rain") {
-            weatherIcon.src = "images/rain.png";
-        }
-        else if(data.weather[0].main == "Mist") {
-            weatherIcon.src = "images/mist.png";
-        }
+        
+        const icon = data.weather[0].main.toLowerCase();
+        weatherIcon.src = `images/${icon}.png`;
+        
         document.querySelector(".error").style.display = "none";
         document.querySelector(".weather").style.display = "block";
     }
     
 }
 
-// Event listener that awaits for the button to be clicked to then use the function to display weather
-searchBtn.addEventListener("click", ()=>{
-    checkWeather(searchBox.value);
+
+// Buttons event listeners
+cBtn.addEventListener("click", () => {
+    unit = "metric";
+    checkWeather(name, unit);
+})
+fBtn.addEventListener("click", () => {
+    unit = "imperial";
+    checkWeather(name, unit);
 })
 
-aaaaaaaaaaaaaaa
+// Event listener that awaits for the button to be clicked to then use the function to display weather
+searchBtn.addEventListener("click", () => {
+    name = searchBox.value;
+    checkWeather(searchBox.value, unit);
+})
+
--- README.md
+++ README.md
@@ -1,17 +1,30 @@
 # js_30_apps
 
 # WEATHER APP
-[Weather png icons source](https://pngtree.com/element/down?id=NDA0OTkxMg==&type=1&time=1719379127&token=YjFjM2VjZjg0ZTBlZGFhM2E2MDY1NmYwNjAwYmYxY2M=&t=0)
-[Search png icon source](https://pngtree.com/element/down?id=Mzk5NDk4Ng==&type=1&time=1719379986&token=ZjEzMjI3ZWFlMTAzNmQ0YTljNjBlZTU4YjMxNTcxZDE=&t=0)
+### Resources
+[Tutorial](https://youtu.be/MIYQR-Ybrn4?si=Y34eSLG0D9RH_H3x) | 
+[Weather png icons source](https://pngtree.com/element/down?id=NDA0OTkxMg==&type=1&time=1719379127&token=YjFjM2VjZjg0ZTBlZGFhM2E2MDY1NmYwNjAwYmYxY2M=&t=0) | 
+[Search png icon source](https://pngtree.com/element/down?id=Mzk5NDk4Ng==&type=1&time=1719379986&token=ZjEzMjI3ZWFlMTAzNmQ0YTljNjBlZTU4YjMxNTcxZDE=&t=0) | 
 [Open Weather API](https://openweathermap.org/current#name)
 
-New things I learned from it: 
-* **CSS**: linear gradient
-* **HTML**: in vscode with automcomplete .name creates div with class, and h1.classname also works; 
 
-Extra things I added:
-* Options to choose from ºF to ºC
-* Changed the way the weather icons are displayed to a more dynamic manner
-* Added extra information to weather card
-* Added option to visualize more than one city/card
-* Fixed bug for when theres more than a city with the same name
+### Project Documentation
+
+#### Estimated Time: 1.5 Hours
+
+### New Concepts Learned:
+
+- **CSS:** Linear gradient for backgrounds.
+- **HTML (VS Code):**
+  - `.name` creates a `div` with class.
+  - `h1.classname` creates an `h1` with class.
+- **JavaScript:**
+  - Working with APIs.
+  - Handling event listeners.
+
+### Additional Features:
+
+- **Temperature Units:** Option to toggle between °F and °C.
+- **Dynamic Weather Icons:** Improved icon-display handling to a more dynamic code form.
+- **Expanded Weather Information:** Added icon description, feels-like temperature, max, and min temperatures.
+
